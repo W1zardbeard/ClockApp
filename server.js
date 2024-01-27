e7bde3c86a8e9e505eb74436a3b75d052789ea67
Tom Main <tommain1498@gmail.com>
2024-01-27 20:04:29 +0000

    added some layout

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import pg from "pg";
+import axios from "axios";
 
 
-
+const apiUrl = "https://api.quotable.io"; 
 const app = express();
 const port = 3000;
 
 export const db = new pg.Client({
     user: "postgres",
     host: "localhost",
-    database: "webDevQuiz",
+    database: "clockApp",
     password: "1234",
     port: 5432,
   });
@@ -21,10 +22,11 @@ app.use([express.json(), express.urlencoded({ extended: true })])
 
 
 
-app.get("/", (req, res) => {
- 
+app.get("/", async (req, res) => {
+    const quote = await axios.get(apiUrl + "/random");
+    console.log(quote.data.content);
     res.render("index.ejs",{
-       
+       quote: quote.data.content
     });
 })
 
