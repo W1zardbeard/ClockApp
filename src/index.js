d6ecf952ad0f7fd286328603c5bc5b399a568ea0
Tom Main <tommain1498@gmail.com>
2024-02-03 17:28:36 +0000

    added show and hide functionality

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import $ from "jquery";
 import axios from "axios";
 
 const quoteApiUrl = "https://api.quotable.io"; 
-
-
+var showBottom = false;
+$(".bottomSheet").hide();
 function time(){
     const now = new Date();
     var hours = now.getHours();
+   
     var minutes = now.getMinutes();
     if(minutes < 10){
         $(".time").text(hours + ":" + "0" + minutes);
@@ -28,7 +29,11 @@ function time(){
 
     if(hours >= 5 && hours < 18){
         $(".dayIcon").attr("src", "../images/desktop/icon-sun.svg");
+        $("main").removeClass("nighttimeBg");
+        $("main").addClass("daytimeBg");
     } else{
+        $("main").removeClass("daytimeBg");
+        $("main").addClass("nighttimeBg");
         $(".dayIcon").attr("src", "../images/desktop/icon-moon.svg");
     }
 }
@@ -55,3 +60,52 @@ $(".refresh").on("click", function(){
     getQuote();
 })
 
+
+// $(".btn").on("click", function(){
+    
+//     if(showBottom == false){
+//         $(".bottomSheet").slideToggle("fast");
+//         setTimeout(function(){
+//             $(".quoteCont").addClass("hide");
+           
+//         }, 500);
+        
+//         showBottom = true;
+//     } else{
+//         $(".quoteCont").removeClass("hide");
+        
+//         setTimeout(function(){
+//             $(".bottomSheet").slideToggle("fast");
+//         }, 500);
+//         showBottom = false;
+//     }
+// })
+
+$(".btn").on("click", function(){
+    if(showBottom == false){
+        $(".bottomSheet").removeClass("animate__slideOutDown");
+        $(".bottomSheet").show();
+        $(".bottomSheet").addClass("animate__slideInUp");
+
+        $(".quoteCont").removeClass("animate__fadeIn");
+        $(".quoteCont").addClass("animate__fadeOut");
+        setTimeout(function(){
+            $(".quoteCont").hide();
+        }, 800);
+
+        showBottom = true;
+    } else{
+        $(".bottomSheet").removeClass("animate__slideInUp");
+        $(".bottomSheet").addClass("animate__slideOutDown");
+        setTimeout(function(){
+            $(".bottomSheet").hide();
+            $(".quoteCont").removeClass("animate__fadeOut");
+            $(".quoteCont").show();
+            $(".quoteCont").addClass("animate__fadeIn");
+        }, 800);
+
+        
+        
+        showBottom = false;
+    }
+})
